030edf2d54b02e53e148a27ccd5e88f8bdc31123
Philip Papurt <philip@emvoiceapp.com>
2018-08-26 19:29:22 -0400

    [frontend] Move queue modification logic to frontend

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 (() => {
-  const escapeReplaceDict = new Map([
+  const escapeHtmlReplace = new Map([
     ['&', '&#38;'],
     ['"', '&#34;'],
     ['\'', '&#39;'],
@@ -7,7 +7,7 @@
     ['>', '&#62;'],
   ])
 
-  const escapeHtml = str => str.replace(/[&"<>']/g, c => escapeReplaceDict.get(c))
+  const escapeHtml = str => str.replace(/[&"<>']/g, c => escapeHtmlReplace.get(c))
 
   let accessToken = ''
   let currentList = []
@@ -15,19 +15,14 @@
     document.getElementById('r').innerHTML = currentList.map((item, idx) => {
       return `<li>
         <span>${escapeHtml(item.name)}</span>
-        <span><em>${escapeHtml(item.priority)}</em></span>
+        <span><em>${escapeHtml(String(item.priority))}</em></span>
         <span id="x${idx}"><strong>x</strong></span>
       </li>`
     }).join('')
     currentList.forEach((_, idx) => {
       document.getElementById(`x${idx}`).addEventListener('click', async () => {
-        await fetch('/.netlify/functions/queue-delete', {
-          method: 'DELETE',
-          headers: {
-            authorization: `Bearer ${accessToken}`,
-          },
-        })
-        await fetchCurrentList()
+        currentList.splice(idx, 1)
+        await updateCurrentList()
       })
     })
   }
@@ -41,20 +36,25 @@
     renderCurrentList()
   }
 
-  document.getElementById('f').addEventListener('submit', async (evt) => {
-    evt.preventDefault()
-    await fetch('/.netlify/functions/queue-add', {
-      method: 'POST',
+  const updateCurrentList = async () => {
+    await fetch('/.netlify/functions/queue-set', {
       headers: {
         authorization: `Bearer ${accessToken}`,
         'content-type': 'application/json',
       },
-      body: JSON.stringify({
-        name: document.getElementById('n').value,
-        priority: document.getElementById('p').value,
-      })
+      body: JSON.stringify(currentList)
     })
     await fetchCurrentList()
+  }
+
+  document.getElementById('f').addEventListener('submit', async (evt) => {
+    evt.preventDefault()
+    currentList.push({
+      name: document.getElementById('n').value,
+      priority: document.getElementById('p').value,
+    })
+    currentList.sort((a, b) => b.priority - a.priority)
+    await updateCurrentList()
   })
 
   const handleUserLogin = async (user) => {
@@ -70,6 +70,7 @@
   netlifyIdentity.on('login', handleUserLogin)
   netlifyIdentity.on('logout', () => {
     currentList = []
+    accessToken = ''
     renderCurrentList()
     document.getElementById('r').hidden = true
     document.getElementById('f').hidden = true
